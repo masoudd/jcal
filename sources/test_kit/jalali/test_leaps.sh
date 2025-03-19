3891c32168895f4827e1fcd4e5c0613ce9c40140
Masoud Naservand <masoud.naservand@gmail.com>
2025-03-19 03:39:01 +0330

    Use ${srcdir} and ${top_srcdir} in Makefile.am
    This makes it possible to build the project in a separate build dir
    like in dir jcal/:
    cd ..; mkdir build; cd build;
    ../jcal/sources/configure;
    make check

--- sources/test_kit/jalali/test_leaps.sh
+++ sources/test_kit/jalali/test_leaps.sh
@@ -1,4 +1,4 @@
 # Source of truth: https://calendar.ut.ac.ir/documents/2139738/7092644/Kabise+Shamsi+1206-1498.pdf
 # Visit https://calendar.ut.ac.ir for more information
 echo "Comparing computed leap years according to https://calendar.ut.ac.ir"
-./list_leap | diff ./leaps_1206_1498.txt -
+./list_leap | diff "${srcdir}/leaps_1206_1498.txt" -
